5dccbabd5281ce3aba84894529e01ef7246f883d
Kevin McKeever <kmckeever@johnsoncountyiowa.gov>
2025-01-22 16:31:46 -0600

    filter dataset

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,51 +1,62 @@
-import  { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Globe from 'react-globe.gl';
 import polygons from './polygons.json';
-
+import earthNight from 'src/assets/earth-night.jpg';
 
 const HomePage = () => {
-  const globeEl = useRef();
-    const [countries, setCountries] = useState(polygons);
-    const [altitude, setAltitude] = useState(0.1);
-    const [transitionDuration, setTransitionDuration] = useState(1000);
+  const globeEl = useRef(undefined);
+  const [countries, setCountries] = useState(polygons);
+  const [altitude, setAltitude] = useState(0.1);
+  const [transitionDuration, setTransitionDuration] = useState(1000);
 
-    useEffect(() => {
-      // load data
-      fetch(polygons).then(res => res.json())
-        .then(countries=> {
-          setCountries(countries);
+  const filteredCountries = ['United States of America', 'India', 'Pakistan', 'Canada', 'Nigeria'];
 
-          setTimeout(() => {
-            setTransitionDuration(4000);
-            setAltitude(() => feat => Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5));
-          }, 3000);
-        });
-    }, []);
+  // useEffect(() => {
+  //   // load data
+  //   fetch(polygons).then(res => res.json())
+  //     .then(countries=> {
+  //       setCountries(countries);
 
-    useEffect(() => {
-      // Auto-rotate
-      globeEl.current.controls().autoRotate = true;
-      globeEl.current.controls().autoRotateSpeed = -1.8;
+  //       setTimeout(() => {
+  //         setTransitionDuration(4000);
+  //         setAltitude(() => feat => Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5));
+  //       }, 3000);
+  //     });
+  // }, []);
 
-      globeEl.current.pointOfView({ altitude: 4 }, 5000);
-    }, []);
+  useEffect(() => {
+    // Auto-rotate
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = -1.8;
+
+    globeEl.current.pointOfView({ altitude: 4 }, 5000);
+  }, []);
 
   return (
     <Globe
-    ref={globeEl}
-    globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-
-    polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
-    polygonAltitude={altitude}
-    polygonCapColor={() => 'rgba(0, 114, 255, 0.6)'}
-    polygonSideColor={() => 'rgba(0, 92, 0, 0.15)'}
-    polygonLabel={({ properties: d }) => <div>
-      <div><b>{d.ADMIN} ({d.ISO_A2})</b></div>
-      <div>Population: <i>{Math.round(+d.POP_EST / 1e4) / 1e2}M</i></div>
-    </div>}
-    polygonsTransitionDuration={transitionDuration}
-  />
-  )
+      ref={globeEl}
+      globeImageUrl={earthNight}
+      polygonsData={countries.features.filter(
+        (d) => d.properties.ISO_A2 !== 'AQ' && filteredCountries.includes(d.properties.ADMIN),
+      )}
+      polygonAltitude={altitude}
+      polygonCapColor={() => 'rgba(0, 114, 255, 0.6)'}
+      polygonSideColor={() => 'rgba(0, 92, 0, 0.15)'}
+      polygonLabel={({ properties: d }) => (
+        <div>
+          <div>
+            <b>
+              {d.ADMIN} ({d.ISO_A2})
+            </b>
+          </div>
+          <div>
+            Population: <i>{Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
+          </div>
+        </div>
+      )}
+      polygonsTransitionDuration={transitionDuration}
+    />
+  );
 };
 
 export default HomePage;
